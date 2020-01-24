94c222167661b7f2d5214ae8ec04108c27c6dbed
Charllieya <charllieya@gmail.com>
2020-01-24 02:02:09 -0600

    game changes

--- js/app3.js
+++ js/app3.js
@@ -10,7 +10,8 @@ var nextButton = document.getElementById('next');
 var homeButton = document.getElementById('home');
 // Create Go Button
 var goButton = document.createElement("input");
-
+// Counter for answers
+var answerCount = 0;
 // Create Welcome Header
 var welcome = document.getElementById('welcome');
 welcome.innerText = 'CHOOSE YOUR DESTINY!';
@@ -122,257 +123,250 @@ function scene3() {
 }
 
 function redPill() {
-    scene.innerHTML = player + ' chose RED PILL';
+    scene.innerHTML = 'RED PILL';
     console.log(player + " You have choosen the red pill.");
     welcome.innerText = '';
     textbox.style.display = "block";
     choice.style.display = 'none';
-    redOne();
-    textboxVariables();
+    red();
+    // textboxVariables();
 }
 
 
 
 //FUNCTION TO START THE GAME FOR THE BLUE PILL
 function bluePill() {
-    scene.innerHTML = player + ' chose BLUE PILL';
+    scene.innerHTML = 'BLUE PILL';
     console.log(player + " You have choosen the blue pill.");
     welcome.innerText = '';
     textbox.style.display = "block";
     choice.style.display = 'none';
-    question.innerText = 'THIS IS A QUESTION';
-    blueOne();
-    textboxVariables();
+    //question.innerText = 'THIS IS A QUESTION';
+    blue();
+    //textboxVariables();
 }
 
 //SCENE FOUR
 //DECLARING TEXT BOX QUESTION AND ANSWER BOXES.
-function textboxVariables() {
+function textboxVariables(answers, question) {
     var scene = document.getElementById('scene');
-    var question = document.getElementById('question');
-    // Declare variable
-    var One;
-    var Two;
-    var Three;
-    var Four;
+    // var question = document.getElementById('question');
 
     // Declare answer variable
     var answerOne = document.getElementById('answer1');
     var answerTwo = document.getElementById('answer2');
-    var answerThree = document.getElementById('answer3');
-    var answerFour = document.getElementById('answer4');
-
-    // Call function to get Question and Answers for scenario 1
-    //RED SCENARIOS
-    //redOne();
-    // redTwo();
-    // redThree();
-    // redFour();
-    // redFive();
-    //BLUE SCENARIOS
-
-    // Set value equal to answers for scenario 1
-    answerOne.setAttribute("value", answers.answerOne);
-    answerTwo.setAttribute("value", answers.answerTwo);
-    answerThree.setAttribute("value", answers.answerThree);
-    answerFour.setAttribute("value", answers.answerFour);
-
-    // Set answer choices equal to answers for current scenario
-    One = document.getElementById('answer1').value;
-    Two = document.getElementById('answer2').value;
-    Three = document.getElementById('answer3').value;
-    Four = document.getElementById('answer4').value;
+    // var answerThree = document.getElementById('answer3');
+    // var answerFour = document.getElementById('answer4');
 
     // Set the innerHTML to the answers for the current scenario
-    answerOne.innerHTML = One;
-    answerTwo.innerHTML = Two;
-    answerThree.innerHTML = Three;
-    answerFour.innerHTML = Four;
+    answerOne.innerHTML = answers.answerOne;
+    answerTwo.innerHTML = answers.answerTwo;
+    // answerThree.innerHTML = answers.answerThree;
+    // answerFour.innerHTML = answers.answerFour;
+
+    // Add 'clicking' to the answers
+    answerOne.addEventListener("click", function() {
+        // console.log("Test: " + question);
+        //If the current question is equal to "Red One: ", then the first answer choice is correct.
+        if (answerCount == 0) {
+            console.log("A. Tom loves your choice in music. He runs into the living with his undies on and starts dancing, and this makes you extremely uncomfortable. You decide to go home. You earn 20 points for getting the song right.");
+            sceneOne();
+        }
+        if (answerCount == 1) {
+            console.log("We all love a good Chaka vibe, but this wasn’t one of those times. Sean just can’t see to get his right feet in motion with his left. He becomes embarrassed and walks away. Kevin, disappointed, tells you that you were no help. You lose 15 points.");
+        }
+        if (answerCount == 2) {
+            console.log("A. Tom loves your choice in music. He runs into the living with his undies on and starts dancing, and this makes you extremely uncomfortable. You decide to go home. You earn 20 points for getting the song right.");
+        }
+        if (answerCount == 3) {
+            console.log("A. Tom loves your choice in music. He runs into the living with his undies on and starts dancing, and this makes you extremely uncomfortable. You decide to go home. You earn 20 points for getting the song right.");
+        }
+        if (answerCount == 4) {
+            console.log("A. Tom loves your choice in music. He runs into the living with his undies on and starts dancing, and this makes you extremely uncomfortable. You decide to go home. You earn 20 points for getting the song right.");
+        }
+    });
+    answerTwo.addEventListener("click", function() {
+        if (answerCount == 0) {
+            console.log("You decide to play Back in Black by AC/DC and this frustrates Tom.  He tells you that he had a surprise for you but didn’t think the music was a perfect fit. You lose ten points.");
+        }
+        if (answerCount == 1) {
+            console.log("Deniece Williams wins every time and we are definitely cheering this boy on. Sean manages to stay on key the entire song. You just earned your  20points. ");
+        }
+        if (answerCount == 2) {
+            console.log("You decide to play Back in Black by AC/DC and this frustrates Tom.  He tells you that he had a surprise for you but didn’t think the music was a perfect fit. You lose ten points.");
+        }
+        if (answerCount == 3) {
+            console.log("You decide to play Back in Black by AC/DC and this frustrates Tom.  He tells you that he had a surprise for you but didn’t think the music was a perfect fit. You lose ten points.");
+        }
+        if (answerCount == 4) {
+            console.log("You decide to play Back in Black by AC/DC and this frustrates Tom.  He tells you that he had a surprise for you but didn’t think the music was a perfect fit. You lose ten points.");
+        }
+    });
+    // answerThree.addEventListener("click", function() {
+    //     if (question == "Red Three: ") {
+    //         console.log("Success!");
+    //     } else {
+    //         console.log("Wrong Answer!");
+    //     }
+    // });
+    // answerFour.addEventListener("click", function() {
+    //     if (question == "Red Four: ") {
+    //         console.log("Success!");
+    //     } else {
+    //         console.log("Wrong Answer!");
+    //     }
+    // });
 }
 //FUNCTION TO START THE GAME FOR THE RED PIll
 
-function redOne() {
-    question.innerText = 'RED ONE: ';
-    answers = {
-            answerOne: 'RED ONE PILL ANSWER ONE',
-            answerTwo: 'RED TWO PILL ANSWER TWO',
-            answerThree: 'RED THREE PILL ANSWER TWO',
-            answerFour: 'RED FOUR PILL ANSWER TWO'
+function red() {
+    // An array of all the questions.
+    let questions = [player + ', Tom cruise invites you to come over. He has the house to himself for a few days. You are excited. The first thing he ask you to do is play a song on the radio so he dance around in the living room. What song do you choose?',
+        'As you are walking home, you see Kevin Bacon trying to teach Sean Penn how to Dance for their senior prom. They stop you and ask you what song should they play to keep Sean on beat. Do you choose:',
+        'Red Three: ',
+        'Red Four: ',
+        'Red Five: '
+    ];
+
+    // Set the question's innerText to the value of the current index. (If you're on the third set of answers, set the question to the question 3, which is 'Red Three').
+    question.innerText = questions[answerCount];
+
+    // An array of objects
+    answers = [
+        // Index 0 of the array. Contains the first set of answer choices.
+        {
+            answerOne: 'Old Time Rock and Roll',
+            answerTwo: 'Back in black.',
+            // answerThree: 'RED ONE PILL ANSWER THREE',
+            // answerFour: 'RED ONE PILL ANSWER FOUR'
+        },
+        // Index 1 of the array. Contains the second set of answer choices.
+        {
+            answerOne: 'Ain’t nobody.',
+            answerTwo: 'Let’s hear it for the Boy',
+            // answerThree: 'RED TWO PILL ANSWER THREE',
+            // answerFour: 'RED TWO PILL ANSWER FOUR'
+        },
+        // Index 2 of the array. Contains the third set of answer choices.
+        {
+            answerOne: 'RED THREE PILL ANSWER ONE',
+            answerTwo: 'RED THREE PILL ANSWER TWO',
+            // answerThree: 'RED THREE PILL ANSWER THREE',
+            // answerFour: 'RED THREE PILL ANSWER FOUR'
+        },
+        // Index 3 of the array. Contains the fourth set of answer choices.
+        {
+            answerOne: 'RED FOUR PILL ANSWER ONE',
+            answerTwo: 'RED FOUR PILL ANSWER TWO',
+            // answerThree: 'RED FOUR PILL ANSWER THREE',
+            // answerFour: 'RED FOUR PILL ANSWER FOUR'
+        },
+        // Index 5 of the array. Contains the fifth set of answer choices.
+        {
+            answerOne: 'RED FIVE PILL ANSWER ONE',
+            answerTwo: 'RED FIVE PILL ANSWER TWO',
+            // answerThree: 'RED FIVE PILL ANSWER THREE',
+            // answerFour: 'RED FIVE PILL ANSWER FOUR'
         }
-        // backButton.addEventListener("click", function() {
-        //     redOne();
-        //     textboxVariables();
-        // });
+    ];
+
+    // Calls the function to create the text boxes. Passes the array of questions and the array of obecjts for the answers (so they don't have to be global variables).
+    textboxVariables(answers[answerCount], questions[answerCount]);
+
+    // Adds 'clicking' to the 'next' button. Goes to the next set of questions/answers.
     nextButton.addEventListener("click", function() {
-        redTwo();
-        textboxVariables();
+        // Increases the current answer choice set by 1 (If you're on answer set 2, it increments by 1, so you can move on to answer set 3).
+        answerCount++;
+        // Calls the function to create the text boxes when the 'next' button is clicked.
+        textboxVariables(answers[answerCount], questions[answerCount]);
+
+        // Updates the question when the 'next' button is clicked.
+        question.innerText = questions[answerCount];
     });
-
-}
-
-
-function redTwo() {
-    question.innerText = 'RED TWO: ';
-    answers = {
-            answerOne: 'RED ONE PILL ANSWER ONE',
-            answerTwo: 'RED TWO PILL ANSWER TWO',
-            answerThree: 'RED THREE PILL ANSWER THREE',
-            answerFour: 'RED FOUR PILL ANSWER FOUR'
-        }
-        // backButton.addEventListener("click", function() {
-        //     redOne();
-        //     textboxVariables();
-        // });
-    nextButton.addEventListener("click", function() {
-        redThree();
-        textboxVariables();
-    });
-}
-
-function redThree() {
-    question.innerText = 'RED THREE: ';
-    answers = {
-            answerOne: 'RED ONE PILL ANSWER ONE',
-            answerTwo: 'RED TWO PILL ANSWER TWO',
-            answerThree: 'RED THREE PILL ANSWER THREE',
-            answerFour: 'RED FOUR PILL ANSWER FOUR'
-        }
-        // backButton.addEventListener("click", function() {
-        //     redTwo();
-        //     textboxVariables();
-        // });
-    nextButton.addEventListener("click", function() {
-        redFour();
-        textboxVariables();
-    });
-}
-
-function redFour() {
-    question.innerText = 'RED FOUR: ';
-    answers = {
-            answerOne: 'RED ONE PILL ANSWER ONE',
-            answerTwo: 'RED TWO PILL ANSWER TWO',
-            answerThree: 'RED THREE PILL ANSWER THREE',
-            answerFour: 'RED FOUR PILL ANSWER FOUR'
-        }
-        // backButton.addEventListener("click", function() {
-        //     redThree();
-        //     textboxVariables();
-        // });
-    nextButton.addEventListener("click", function() {
-        redFive();
-        textboxVariables();
-    });
-}
-
-function redFive() {
-    question.innerText = 'RED FIVE: ';
-    answers = {
-            answerOne: 'RED ONE PILL ANSWER ONE',
-            answerTwo: 'RED TWO PILL ANSWER TWO',
-            answerThree: 'RED THREE PILL ANSWER THREE',
-            answerFour: 'RED FOUR PILL ANSWER FOUR'
-        }
-        // backButton.addEventListener("click", function() {
-        //     redFour();
-        //     textboxVariables();
-        // });
     homeButton.addEventListener("click", function() {
         begin();
     });
-
 }
 
-/**************************************************************************************************************************************************************** */
-function blueOne() {
-    question.innerText = 'BLUE ONE: ';
-    answers = {
-            answerOne: 'Get off',
-            answerTwo: 'Get on',
-            answerThree: 'get back',
-            answerFour: 'hello'
+function blue() {
+    let questions = ['Charllieya , Tom cruise invites you to come over. He has the house to himself for a few days. You are excited. The first thing he ask you to do is play a song on the radio so he dance around in the living room. What song do you choose?',
+        'As you are walking home, you see Kevin Bacon trying to teach Sean Penn how to Dance for their senior prom. They stop you and ask you what song should they play to keep Sean on beat. Do you choose:',
+        'Red Three: ',
+        'Red Four: ',
+        'Red Five: '
+    ];
+
+    // Set the question's innerText to the value of the current index. (If you're on the third set of answers, set the question to the question 3, which is 'Red Three').
+    question.innerText = questions[answerCount];
+
+    // An array of objects
+    answers = [
+        // Index 0 of the array. Contains the first set of answer choices.
+        {
+            answerOne: 'Old Time Rock and Roll',
+            answerTwo: 'Back in black.',
+            // answerThree: 'RED ONE PILL ANSWER THREE',
+            // answerFour: 'RED ONE PILL ANSWER FOUR'
+        },
+        // Index 1 of the array. Contains the second set of answer choices.
+        {
+            answerOne: 'Ain’t nobody.',
+            answerTwo: 'Let’s hear it for the Boy',
+            // answerThree: 'RED TWO PILL ANSWER THREE',
+            // answerFour: 'RED TWO PILL ANSWER FOUR'
+        },
+        // Index 2 of the array. Contains the third set of answer choices.
+        {
+            answerOne: 'RED THREE PILL ANSWER ONE',
+            answerTwo: 'RED THREE PILL ANSWER TWO',
+            // answerThree: 'RED THREE PILL ANSWER THREE',
+            // answerFour: 'RED THREE PILL ANSWER FOUR'
+        },
+        // Index 3 of the array. Contains the fourth set of answer choices.
+        {
+            answerOne: 'RED FOUR PILL ANSWER ONE',
+            answerTwo: 'RED FOUR PILL ANSWER TWO',
+            // answerThree: 'RED FOUR PILL ANSWER THREE',
+            // answerFour: 'RED FOUR PILL ANSWER FOUR'
+        },
+        // Index 5 of the array. Contains the fifth set of answer choices.
+        {
+            answerOne: 'RED FIVE PILL ANSWER ONE',
+            answerTwo: 'RED FIVE PILL ANSWER TWO',
+            // answerThree: 'RED FIVE PILL ANSWER THREE',
+            // answerFour: 'RED FIVE PILL ANSWER FOUR'
         }
-        // backButton.addEventListener("click", function() {
-        //     blueOne();
-        //     textboxVariables();
-        // });
+    ];
+
+    // Calls the function to create the text boxes. Passes the array of questions and the array of obecjts for the answers (so they don't have to be global variables).
+    textboxVariables(answers[answerCount], questions[answerCount]);
+
+    // Adds 'clicking' to the 'next' button. Goes to the next set of questions/answers.
     nextButton.addEventListener("click", function() {
-        blueTwo();
-        textboxVariables();
-    });
-}
+        // Increases the current answer choice set by 1 (If you're on answer set 2, it increments by 1, so you can move on to answer set 3).
+        answerCount++;
+        // Calls the function to create the text boxes when the 'next' button is clicked.
+        textboxVariables(answers[answerCount], questions[answerCount]);
 
-function blueTwo() {
-    question.innerText = 'BLUE TWO: ';
-    answers = {
-            answerOne: 'yell',
-            answerTwo: '2',
-            answerThree: 'get back',
-            answerFour: 'hello'
-        }
-        // backButton.addEventListener("click", function() {
-        //     blueOne();
-        //     textboxVariables();
-        // });
-    nextButton.addEventListener("click", function() {
-        blueThree();
-        textboxVariables();
+        // Updates the question when the 'next' button is clicked.
+        question.innerText = questions[answerCount];
     });
-}
-
-function blueThree() {
-    question.innerText = 'BLUE THREE: ';
-    answers = {
-            answerOne: 'yell',
-            answerTwo: '3',
-            answerThree: 'get back',
-            answerFour: 'hello'
-        }
-        // backButton.addEventListener("click", function() {
-        //     blueTwo();
-        //     textboxVariables();
-        // });
-    nextButton.addEventListener("click", function() {
-        blueFour();
-        textboxVariables();
-    });
-}
-
-function blueFour() {
-    question.innerText = 'BLUE FOUR: ';
-    answers = {
-            answerOne: 'yell',
-            answerTwo: '4',
-            answerThree: 'get back',
-            answerFour: 'hello'
-        }
-        // backButton.addEventListener("click", function() {
-        //     blueThree();
-        //     textboxVariables();
-        // });
-    nextButton.addEventListener("click", function() {
-        blueFive();
-        textboxVariables();
-    });
-}
-
-function blueFive() {
-    question.innerText = 'BLUE FIVE: ';
-    answers = {
-            answerOne: 'yell',
-            answerTwo: '5',
-            answerThree: 'get back',
-            answerFour: 'hello'
-        }
-        // backButton.addEventListener("click", function() {
-        //     blueFour();
-        //     textboxVariables();
-        // });
     homeButton.addEventListener("click", function() {
         begin();
     });
-
 }
-/** */
-/* HI CHARLLIEYA */
 /*****************************************************************************************************************************************************************/
+
+var answerOne = document.getElementById('answer1');
+var answerTwo = document.getElementById('answer2');
+
+function sceneOne() {
+    question.innerText = 'A. Tom loves your choice in music. He runs into the living with his undies on and starts dancing, and this makes you extremely uncomfortable. You decide to go home. You earn 20 points for getting the song right. ';
+    answerOne.style.display = 'none';
+    answerTwo.style.display = 'none';
+    nextButton.addEventListener("click", function() {
+        answerOne.style.display = 'inline';
+        answerTwo.style.display = 'inline';
+    });
+    // answerOne.style.display = 'block';
+}
